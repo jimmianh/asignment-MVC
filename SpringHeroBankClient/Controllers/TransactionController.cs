8f76e596e3c096fbc4b9cadff10f4f31fa07c967
anh s <jimmianh1807@gmail.com>
2022-04-29 23:02:34 +0700

    commit

---
+++ SpringHeroBankClient/Controllers/TransactionController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SpringHeroBankClient.ServiceReference1;
+
+namespace SpringHeroBankClient.Controllers
+{
+
+    public class TransactionController : Controller
+    {
+      
+
+        Service1Client service;
+
+        public TransactionController()
+        {
+            service = new Service1Client();
+        }
+
+        public ActionResult Deposit()
+        {
+           
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DepositPost(String token, double Amount)
+        {
+
+
+            if (service.Deposit(token, Amount) == null)
+            {
+                return View("Failed");
+            }
+            else
+            {
+                return View("Success");
+            }
+        }
+
+        public ActionResult Withdraw()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Withdraw(string token, double Amount)
+        {
+
+            if (service.Deposit(token, Amount) == null)
+            {
+                return View("Failed");
+            }
+            else
+            {
+                return View("Success");
+            }
+        }
+        public ActionResult Transfer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Transfer(string receiverAccountNumber, double amount)
+        {
+            var service = new Service1Client();
+            var token = Views.AccountController.staticToken;
+
+            if (service.Transfer(token, receiverAccountNumber, amount))
+            {
+                ViewBag.Message = "Action success";
+            }
+            else
+            {
+                ViewBag.Message = "Action failed";
+            }
+            return View();
+        }
+    }
+}
---
+++ SpringHeroBankClient/Models/TransactionHistory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SpringHeroBankClient.Models
+{
+    public class TransactionHistory
+    {
+        public string Id { get; set; }
+
+        public int Type { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+
+        public string SenderAccountNumber { get; set; }
+
+        [Required]
+        public string ReceiverAccountNumber { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
